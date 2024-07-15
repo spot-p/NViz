c5dab91d09bb62168725a5f26f1b935c22cd10a3
spotdar <spotdar@wgu.edu>
2024-07-14 21:29:33 -0400

    updated plot.js

--- js/plot.js
+++ js/plot.js
@@ -451,7 +451,7 @@ function setupScene(scene) {
 }
 }
 
-d3.csv('/data/aggregate_data.csv').then(data => {
+d3.csv('./data/aggregate_data.csv').then(data => {
     window.data = data;
 
     data.forEach(d => {
